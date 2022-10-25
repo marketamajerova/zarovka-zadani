e34285b53c3fe29359e3fd9303d26ae1bec2d6f9
marketamajerova <marketamajerova.6@gmail.com>
2022-10-25 20:46:44 +0200

    3 bulbs lighting

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('funguju!');
+import { Bulb } from './Bulb/index.js';
+
+const app = document.getElementById('app');
+
+app.append(Bulb({
+        isOn: false,
+    }),
+    Bulb({
+        isOn: true,
+    }),
+    Bulb({
+        isOn: true,
+    })
+);
